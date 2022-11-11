67f0425a97444e8a8f3d6e1b3c274bb7910749d4
Ilya Shchekin <komradaxon@gmail.com>
2022-11-11 09:12:39 +0300

    add google analytics

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -52,6 +52,10 @@ const config = {
         theme: {
           customCss: require.resolve('./src/css/custom.css'),
         },
+        googleAnalytics: {
+          trackingID: 'G-QH27QWY0H1',
+          anonymizeIP: true,
+        },
       }),
     ],
   ],
